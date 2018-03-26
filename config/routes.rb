52e53c82458d82f742f59ae092eb50a6908c404d
Simon Lefebvre <sim8722@hotmail.com>
2018-03-26 16:19:27 +0200

    exo 1 fini

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root to: "questions#ask"
+  # get 'questions/ask'
+
+  # get 'questions/answer'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'ask', to: 'questions#ask'
+  get 'answer', to: 'questions#answer'
 end
